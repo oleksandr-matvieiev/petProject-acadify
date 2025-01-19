7e64301d5bb48c79fb58ff13b24a5c9a9cd17628
Oleksandr <oleksandr.matvieiev.d@gmail.com>
2025-01-19 12:32:26 +0100

    feat: Added jwt token provider

---
+++ src/main/java/org/example/acadify/Security/JwtTokenProvider.java
@@ -0,0 +1,67 @@
+package org.example.acadify.Security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.List;
+
+@Component
+public class JwtTokenProvider {
+    Logger log = LoggerFactory.getLogger(JwtTokenProvider.class);
+
+    @Value("${app.jwt.secret}")
+    private String jwtSecret;
+
+    @Value("${app.jwt.expiration}")
+    private long jwtExpirationInMs;
+
+    public String generateToken(String username, List<String> roles) {
+        List<String> formattedRoles = roles.stream()
+                .map(role -> "ROLE_" + role).toList();
+
+        Date now = new Date();
+        Date expiryDate = new Date(now.getTime() + jwtExpirationInMs);
+
+        return Jwts.builder()
+                .setSubject(username)
+                .claim("roles", formattedRoles)
+                .setIssuedAt(now)
+                .setExpiration(expiryDate)
+                .signWith(SignatureAlgorithm.HS512, jwtSecret)
+                .compact();
+    }
+
+    public String getUsernameFromToken(String token) {
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(jwtSecret)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+        return claims.getSubject();
+    }
+
+    public List<String> getRolesFromToken(String token) {
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(jwtSecret)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+        return claims.get("roles", List.class);
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder().setSigningKey(jwtSecret).build().parseClaimsJws(token);
+            return true;
+        } catch (Exception ex) {
+            log.error("Unexpected error while validating JWT: {}", ex.getMessage());
+        }
+        return false;
+    }
+}
